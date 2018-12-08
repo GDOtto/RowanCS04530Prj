b9e88abdb048c24f50d01449be156a20f1d31bad
GDOtto <devineg@yahoo.com>
2018-12-08 10:04:10 -0500

    Added searchForFood

--- src/edamam.py
+++ src/edamam.py
@@ -25,6 +25,7 @@ if not os.path.isfile(edamam_config_file):
 edamam_config.read(edamam_config_file)
 
 config_recipe_api = edamam_config['RecipeAPI']
+config_food_api = edamam_config['FoodAPI']
 
 def searchForRecipes(keyword):
     print("calling")
@@ -50,10 +51,35 @@ def searchForRecipes(keyword):
     
     return r.json
 
+def searchForFood(keyword):
+    print("calling")
+    #create request payload that includes the app id and key.
+    # this is a search and reference the Search call 
+    # more details here https://developer.edamam.com/edamam-docs-recipe-api
+    payload = {'ingr'       : keyword, 
+               'app_id'  : config_food_api['app_id'], 
+               'app_key' : config_food_api['app_key']}
+    
+    #use requests library
+    r = requests.get('https://api.edamam.com/api/food-database/parser', params=payload)
+
+    #dump to file to look at
+    f=io.open( "../data/sample_FoodAPIParseResults.txt", "a+", encoding="utf-8")
+#    f=open("../data/sample_RecipeAPISearchResults.txt", "a+")
+    try:
+        f.write("#######\r\n")
+        f.write(r.text)
+    finally:
+        f.close()
+    
+    
+    return r.json
+    
 
 
 if __name__ == "__main__":
     #Test module
     
-    searchForRecipes("duck")
+    #searchForRecipes("duck")
+    searchForFood("1 can diced tomatoes")
 
