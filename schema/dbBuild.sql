6145958cdb5dce19dd3972c08e64d6769af5d261
demete43 <44590453+demete43@users.noreply.github.com>
2018-12-10 22:21:12 -0500

    Update dbBuild.sql

--- schema/dbBuild.sql
+++ schema/dbBuild.sql
@@ -5,82 +5,43 @@
 #DROP database Food;
 #CREATE database food;
 
-
 USE food;
 
 DROP TABLE IF EXISTS `shopping` ;
 DROP TABLE IF EXISTS `ingredients` ;
-DROP TABLE IF EXISTS `nutrition` ;
-DROP TABLE IF EXISTS `directions` ;
+DROP TABLE IF EXISTS `nutrients` ;
 DROP TABLE IF EXISTS `recipes` ;
-
 
 CREATE TABLE recipes
 (
-  recipeID	INT	PRIMARY KEY   AUTO_INCREMENT,
-  RecName		VARCHAR(50)     NOT NULL,
-  servings	VARCHAR(50)     NOT NULL,
-  diet	VARCHAR(50)     NOT NULL
-);
-
-CREATE TABLE nutrition
-(
-  ingredientID	INT PRIMARY KEY AUTO_INCREMENT,
-  name			VARCHAR(75)	NOT NULL,
-  Energy_kcal	DOUBLE,
-  Carbs_g		DOUBLE,
-  Fat_g			DOUBLE,
-  Fiber_g		DOUBLE,
-  Protien_g		DOUBLE
+  recipeID	  INT	PRIMARY KEY AUTO_INCREMENT,
+  recipeName	VARCHAR(50) NOT NULL,
+  servings	  VARCHAR(50) NOT NULL,
+  diet	      VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE ingredients
 (
-<<<<<<< HEAD
-  ingredientID	INT,
-  recipeID		INT,
-  qty			FLOAT,
-  CONSTRAINT `RecipeID`
-    FOREIGN KEY (`RecipeID`)
-    REFERENCES `recipes` (`RecipeID`),
-  CONSTRAINT `ingredientID`
-    FOREIGN KEY (`ingredientID`)
-    REFERENCES `nutrition` (`ingredientID`)
-=======
-  nutritiantID		INT,
-  recipeID		INT,
-  qty			double
->>>>>>> 84acf03b933baaf98e1f7a21a3f62e1e4d38f90c
+  nutrientID  INT,
+  recipeID	  INT,
+  foodQty     DOUBLE
 );
 
-CREATE TABLE nutritiant
+CREATE TABLE nutrients
 (
-  nutritiantID	INT	PRIMARY KEY	AUTO_INCREMENT,
-  FoodName		VARCHAR(75)	NOT NULL,
-  calories	double,
-  fat		double,
-  carb		double,
-  fiber		double,
-  protein	double,
-  UPC		varchar(12)
+  nutrientID  	INT	PRIMARY KEY	AUTO_INCREMENT,
+  foodName		  VARCHAR(75)	NOT NULL,
+  calories	    DOUBLE,
+  fat		        DOUBLE,
+  carbs		      DOUBLE,
+  fiber		      DOUBLE,
+  protein	      DOUBLE,
+  UPC		        VARCHAR(12)
 );
-
-<<<<<<< HEAD
 
 CREATE TABLE shopping
 (
-  ingredientID	INT,
-  qty			INT,
-  store			VARCHAR(0)	NOT NULL,
-  price			DOUBLE,
-  CONSTRAINT `ingredientID2`
-    FOREIGN KEY (`ingredientID`)
-    REFERENCES `ingredients` (`ingredientID`)
-=======
-CREATE TABLE shopping
-(
-  UPC		varchar(12) PRIMARY KEY,
-  qty		double,
-  price		double
->>>>>>> 84acf03b933baaf98e1f7a21a3f62e1e4d38f90c
+  UPC   VARCHAR(12) PRIMARY KEY,
+  size  DOUBLE,
+  price	DOUBLE
 );
