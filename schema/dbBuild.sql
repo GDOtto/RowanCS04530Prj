a0560aad6798a0246ec103f47ed118b65eb29553
demete43 <44590453+demete43@users.noreply.github.com>
2018-12-10 22:37:36 -0500

    cleaned up schema and created EER model

--- schema/dbBuild.sql
+++ schema/dbBuild.sql
@@ -14,29 +14,29 @@ DROP TABLE IF EXISTS `recipes` ;
 
 CREATE TABLE recipes
 (
-  recipeID	  INT	PRIMARY KEY AUTO_INCREMENT,
+  recipeID		INT	PRIMARY KEY AUTO_INCREMENT,
   recipeName	VARCHAR(50) NOT NULL,
-  servings	  VARCHAR(50) NOT NULL,
-  diet	      VARCHAR(50) NOT NULL
+  servings		VARCHAR(50) NOT NULL,
+  diet			VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE ingredients
 (
-  nutrientID  INT,
-  recipeID	  INT,
+  nutrientID  INT REFERENCES nutrients(nutrientID),
+  recipeID	  INT REFERENCES recipes(recipeID),
   foodQty     DOUBLE
 );
 
 CREATE TABLE nutrients
 (
-  nutrientID  	INT	PRIMARY KEY	AUTO_INCREMENT,
-  foodName		  VARCHAR(75)	NOT NULL,
-  calories	    DOUBLE,
-  fat		        DOUBLE,
-  carbs		      DOUBLE,
-  fiber		      DOUBLE,
-  protein	      DOUBLE,
-  UPC		        VARCHAR(12)
+  nutrientID	INT	PRIMARY KEY	AUTO_INCREMENT,
+  foodName		VARCHAR(75)	NOT NULL,
+  calories		DOUBLE,
+  fat			DOUBLE,
+  carbs			DOUBLE,
+  fiber			DOUBLE,
+  protein		DOUBLE,
+  UPC			VARCHAR(12)
 );
 
 CREATE TABLE shopping
