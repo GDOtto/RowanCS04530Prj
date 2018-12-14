9321193ed322756cb48e0756ad551a596b724598
GDOtto <devineg@yahoo.com>
2018-12-14 06:59:03 -0500

    Added UPC and size

---
+++ schema/addUPC.sql
@@ -0,0 +1,26 @@
+
+UPDATE nutrients SET UPC="052100030104" WHERE nutrientID = "1";
+UPDATE nutrients SET UPC="052100001241" WHERE nutrientID = "2";
+UPDATE nutrients SET UPC="032917002235" WHERE nutrientID = "3";
+UPDATE nutrients SET UPC="078742101323" WHERE nutrientID = "4";
+UPDATE nutrients SET UPC="078742131795" WHERE nutrientID = "5";
+UPDATE nutrients SET UPC="078742254463" WHERE nutrientID = "6";
+UPDATE nutrients SET UPC="072412010175" WHERE nutrientID = "7";
+UPDATE nutrients SET UPC="044700072615" WHERE nutrientID = "8";
+-- UPDATE nutrients SET UPC="" WHERE nutrientID = "9";
+UPDATE nutrients SET UPC="089836184665" WHERE nutrientID = "10";
+UPDATE nutrients SET UPC="016000106109" WHERE nutrientID = "11";
+UPDATE nutrients SET UPC="049200042014" WHERE nutrientID = "12";
+UPDATE nutrients SET UPC="019900338021" WHERE nutrientID = "13";
+UPDATE nutrients SET UPC="052100002569" WHERE nutrientID = "14";
+UPDATE nutrients SET UPC="052100004389" WHERE nutrientID = "15";
+UPDATE nutrients SET UPC="078742353388" WHERE nutrientID = "16";
+UPDATE nutrients SET UPC="078742046990" WHERE nutrientID = "17";
+UPDATE nutrients SET UPC="742365004346" WHERE nutrientID = "18";
+UPDATE nutrients SET UPC="041224910164" WHERE nutrientID = "19";
+UPDATE nutrients SET UPC="074265001577" WHERE nutrientID = "20";
+UPDATE nutrients SET UPC="044000000578" WHERE nutrientID = "21";
+UPDATE nutrients SET UPC="089836184290" WHERE nutrientID = "22";
+UPDATE nutrients SET UPC="042563009762" WHERE nutrientID = "23";
+UPDATE nutrients SET UPC="113102857967" WHERE nutrientID = "24";
+select * FROM nutrients
